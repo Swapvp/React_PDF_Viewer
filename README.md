1fc7a0824dc3318020c1594708c07672245e4907
Swapvp <swapneshpawar1994@gmail.com>
2024-05-26 11:12:45 +0530

    pdf-viewer

---
+++ README.md
@@ -0,0 +1,1 @@
+# React PDF Viewer
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import PdfViewComponent from "./Components/PdfViewComponent";
+import "./App.css";
+
+function App() {
+  return <PdfViewComponent />;
+}
+
+export default App;
